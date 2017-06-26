95d3d35e463b9c4399b880d866a3ede48e47937d
Stefan Piazza <stefanpiazza@gmail.com>
2017-06-26 12:48:39 +0100

    UPDATE: index.html added as template for plugin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,6 +3,7 @@
 var styles = require('../styles/app.scss');
 var pixel = require('../images/pixel.jpg');
 var favicon = require('../images/favicon.png');
+var testImage = require('../images/test.jpg');
 
 (function() {
 
