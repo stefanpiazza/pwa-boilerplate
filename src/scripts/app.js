3fb7ee0fade340a7c05630748f2a11bf061b69ac
Stefan Piazza <stefanpiazza@gmail.com>
2017-06-29 15:29:31 +0100

    UPDATE: Remove extra requires

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 var styles = require('../styles/app.scss');
-var pixel = require('../images/pixel.jpg');
-var favicon = require('../images/favicon.png');
-var testImage = require('../images/test.jpg');
 
 (function() {
 
