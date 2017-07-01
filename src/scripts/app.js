d71c4638be1526143b732013eadbf20ace77a155
Stefan Piazza <stefanpiazza@gmail.com>
2017-07-01 16:55:59 +0100

    UPDATE: Import react and render basic element

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,22 @@
 'use strict';
 
 var styles = require('../styles/app.scss');
+// Imported styles can be referenced
+// e.g. className = {styles.title}
+
+import React from 'react';
+import {
+    render
+} from 'react-dom';
 
 (function() {
+    class App extends React.Component {
+        render() {
+            return (
+                <h1>Hello, world!</h1>
+            );
+        }
+    }
 
+    render(<App />, document.getElementById('app'));
 })();
