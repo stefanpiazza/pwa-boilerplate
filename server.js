bb90a79435f90145ba15e505c7b07b07cd83d49b
Stefan Piazza <stefanpiazza@gmail.com>
2017-07-22 19:22:09 +0100

    ADD: Basic express server that always renders index.html, routing handled by react router

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+
+app.use(express.static(path.resolve(__dirname, 'app')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'app', 'index.html'));
+});
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, function() {
+
+});
